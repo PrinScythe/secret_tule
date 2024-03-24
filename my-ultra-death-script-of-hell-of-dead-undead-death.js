1f77f302f87999ce438ca83aa481ce59ad62199c
prinscythe <prinscythe@outlook.fr>
2024-03-24 08:23:45 +0100

    uwu

--- my-ultra-death-script-of-hell-of-dead-undead-death.js
+++ my-ultra-death-script-of-hell-of-dead-undead-death.js
@@ -1,35 +1,45 @@
-const PROMPT_NAME = 'myPrompt';
-const BLOCK = ["⠀", "▏","▎","▍","▌","▋","▊","▉","█"];
-
-function getProgressBar(percent, lenght = 10) {
-    if (lenght <= 0) return "error";
-    if (percent < 0 || percent > 100) return "error";
-    const p = (percent / 100) * lenght;
-    const nbOfFullBlocks = Math.floor(p);
-    const nbOfEmptyBlocks = Math.floor(lenght - p);
-    const x = Math.floor((p - nbOfFullBlocks) * 9);
-    const progressbar = BLOCK[8].repeat(nbOfFullBlocks) + BLOCK[x] + BLOCK[0].repeat(nbOfEmptyBlocks);
-    return progressbar + percent + "%";
-}
-
-function printInPrompt(text = "Hello Wolrd"){
-    document.getElementById(PROMPT_NAME).innerHTML = text;
-}
-
-function buttonFun() {
-    const percent = document.getElementById('percent').value;
-    const lenght = document.getElementById('lenght').value;
-    printInPrompt(getProgressBar(percent, lenght));
-}
-
-function autoPlay(step = 0) {
-    if(step == 100.01) {}
-    else {
-        const lenght = document.getElementById('lenght').value;
-        printInPrompt(getProgressBar(step, lenght));
-        sleep(200).then(() => {
-            // Do something after the sleep!
-            autoPlay(step + 0.01)
+const hashValue = val =>
+    crypto.subtle
+        .digest('SHA-256', new TextEncoder('utf-8').encode(val))
+        .then(h => {
+            let hexes = [],
+                view = new DataView(h);
+            for (let i = 0; i < view.byteLength; i += 4)
+                hexes.push(('00000000' + view.getUint32(i).toString(16)).slice(-8));
+            return hexes.join('');
         });
-    }
+
+function dosomething() {
+    document.getElementById('error1').classList.add('errorHide');
+    document.getElementById('error1').classList.remove('error');
+    document.getElementById('error2').classList.add('errorHide');
+    document.getElementById('error2').classList.remove('error');
+
+    const code = document.getElementById("code").value;
+    const heil = document.querySelector('input[name="heil"]:checked')?.value;
+    if (code == null || code.trim() == '' || heil == null || heil.trim() == '') {
+        if (code == null || code.trim() == '') {
+            document.getElementById('error1').classList.remove('errorHide');
+            document.getElementById('error1').classList.add('error');
+        }
+        if (heil == null || heil.trim() == '') {
+            document.getElementById('error2').classList.remove('errorHide');
+            document.getElementById('error2').classList.add('error');
+        }
+        return;
+    };
+
+    hashValue(code).then(codeHash => {
+        if (codeHash == '65e29476f84d3bb4c2df04b3f59b3b02f192104a5d76e85eb04bcd2b499086e6')
+            hashValue(heil).then(heilHash => {
+                if (heilHash == 'dc03c80655fc9c5a1a7fcf0e02c27407a29550e88fab3a1b9acc9986b29d40be')
+                    window.location.replace("https://youtu.be/isyMHz6pgp4");
+                else
+                    window.location.replace("https://youtu.be/ss4bdWou8dY");
+            })
+        else {
+            document.getElementById('error1').classList.remove('errorHide');
+            document.getElementById('error1').classList.add('error');
+        }
+    });
 }
